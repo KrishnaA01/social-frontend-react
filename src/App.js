7952024ec99b99d28f2ddfc1ffcacdcc74c6314b
KrishnaA01 <mahato77@hotmail.com>
2022-01-16 20:26:48 +0100

    created react app for social media frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './pages/home/Home';
+import PersonIcon from '@mui/icons-material/Person';
+
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
+
+//https://www.youtube.com/watch?v=zM93yZ_8SvE&t=117s
