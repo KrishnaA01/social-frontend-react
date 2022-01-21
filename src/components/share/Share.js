2c61a86fd657aa613f20e16f8e46cd7e3aa73526
KrishnaA01 <mahato77@hotmail.com>
2022-01-21 23:00:27 +0100

    feed done

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,5 +1,8 @@
 import "./share.css";
 import PermMediaIcon from '@mui/icons-material/PermMedia';
+import LabelIcon from '@mui/icons-material/Label';
+import RoomIcon from '@mui/icons-material/Room';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 export default function Share() {
   return (
@@ -14,10 +17,23 @@ export default function Share() {
             <div className="shareBottom">
                 <div className="shareOptions">
                     <div className="shareOption">
-                    <PermMediaIcon className="shareIcon"/>
+                    <PermMediaIcon style={{color:"tomato"}} className="shareIcon"/>
                         <span className="shareOptionText">Photo or Videos</span>
                     </div>
+                    <div className="shareOption">
+                    <LabelIcon style={{color:"blue"}} className="shareIcon"/>
+                        <span className="shareOptionText">Tag</span>
+                    </div>
+                    <div className="shareOption">
+                    <RoomIcon style={{color:"green"}} className="shareIcon"/>
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className="shareOption">
+                    <EmojiEmotionsIcon style={{color:"gold"}} className="shareIcon"/>
+                        <span className="shareOptionText">Feelings</span>
+                    </div>
                 </div>
+                <button className="shareButton">Share</button>
             </div>
          
       </div>
